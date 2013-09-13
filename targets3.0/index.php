738bd2575b6aee70748958b720cc41616049644d
OpenShift guest <eaglesmaysoarbutweaslesdontgetsuckedintojetengines@hotmail.com>
2013-09-13 06:40:11 -0400

    general update

--- targets3.0/index.php
+++ targets3.0/index.php
@@ -12,27 +12,95 @@
 			$targets = explode("," , $temp[0]);
 			$remaining = explode("," , $temp[1]);
 		?>
+		<div class='title'>
+		    <h1>Target Counter 3.0</h1>
+		</div>
+		<div class='teamSelect'>
+		    <label>Team:
+		        <select id='teamSelect'>
+		            <option value='ana'>Analytical</option>
+		            <option value='bio'>Biological</option>
+		            <option value='gen'>General</option>
+		            <option value='inorg'>Inorganic</option>
+		            <option value='int'>Interfaces</option>
+		            <option value='mat'>Materials</option>
+		            <option value='org'>Organic</option>
+		            <option value='app'>Organic Applications</option>
+		            <option value='phys'>Physical</option>
+		        </select>
+		    </label>
+		    <button id='defaultTeamButton'>Set as default team</button>
+		</div>
+		<div class="admin">
+		    <button id="showControl">Show Admin Controls</button>
+		    <div id="control" hidden>
+		        <h3>Enter this week's targets:</h3>
+		        <label>New Manuscripts:
+		            <input id="inputPeerReview" />
+		        </label>
+		        <br/>
+		        <label>Team Edits:
+		            <input id="inputTeamEdits" />
+		        </label>
+		        <br/>
+		        <label>Non-Team Edits:
+		            <input id="inputNonTeamEdits" />
+		        </label>
+		        <br/>
+		        <label>Team Proofs:
+		            <input id="inputTeamProofs" />
+		        </label>
+		        <br/>
+		        <label>Non-Team Proofs:
+		            <input id="inputNonTeamProofs" />
+		        </label>
+		        <br/>
+		        <button id="resetTargets">Reset Targets</button>
+		        <button id="updateTargets">Update Targets</button>
+		    </div>
+		</div>
 		<div class='peerReview'>
-			<h2>Peer Review</h2>
-			<span id='peerReviewTarget'>Target: <?php echo $targets[0]; ?></span><br/>
-			<span id='peerReviewRemaining'>Remaining: <?php echo $remaining[0]; ?></span><br/>
-			<input type='text' id='peerReviewAmount' placeholder='1'/><br/>
-			<a id='peerReviewButton' href='action.php?task=peerReview&amount=1' class='button'>Decrement</a>
+		    <h2>Peer Review</h2>
+		    <span id='peerReviewTarget'>Target:
+		        <?php echo $targets[0]; ?>
+		    </span>
+		    <br/>
+		    <span id='peerReviewRemaining'>Remaining:
+		        <?php echo $remaining[0]; ?>
+		    </span>
+		    <br/>
+		    <input type='text' id='peerReviewAmount' placeholder='1' />
+		    <br/>
+		    <a id='peerReviewButton' href='action.php?task=peerReview&amount=1' class='button'>Decrement</a>
 		</div>
 		<div class='editing'>
-			<h2>Editing</h2>
-			<span id='peerReviewTarget'>Target: <?php echo $targets[1]; ?></span><br/>
-			<span id='peerReviewRemaining'>Remaining: <?php echo $remaining[1]; ?></span><br/>
-			<input type='text' id='editingAmount' placeholder='1'/><br/>
-			<a href='action.php?task=editing&amount=1' id='editingButton' class='button'>Decrement</a>
+		    <h2>Editing</h2>
+		    <span id='peerReviewTarget'>Target:
+		        <?php echo $targets[1]; ?>
+		    </span>
+		    <br/>
+		    <span id='peerReviewRemaining'>Remaining:
+		        <?php echo $remaining[1]; ?>
+		    </span>
+		    <br/>
+		    <input type='text' id='editingAmount' placeholder='1' />
+		    <br/>
+		    <a href='action.php?task=editing&amount=1' id='editingButton' class='button'>Decrement</a>
 		</div>
 		<div class='proofing'>
-			<h2>Proofing</h2>
-			<span id='peerReviewTarget'>Target: <?php echo $targets[2]; ?></span><br/>
-			<span id='peerReviewRemaining'>Remaining: <?php echo $remaining[2]; ?></span><br/>
-			<input type='text' id='proofingAmount' placeholder='1'/><br/>
-			<a href='action.php?task=proofing&amount=1' id='proofingButton' class='button'>Decrement</a>
+		    <h2>Proofing</h2>
+		    <span id='peerReviewTarget'>Target:
+		        <?php echo $targets[2]; ?>
+		    </span>
+		    <br/>
+		    <span id='peerReviewRemaining'>Remaining:
+		        <?php echo $remaining[2]; ?>
+		    </span>
+		    <br/>
+		    <input type='text' id='proofingAmount' placeholder='1' />
+		    <br/>
+		    <a href='action.php?task=proofing&amount=1' id='proofingButton' class='button'>Decrement</a>
 		</div>
 	</body>
-	
+		
 </html>
--- targets3.0/js/main.js
+++ targets3.0/js/main.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
 	$('input').change(function(){
 		update(event);
 	});
+	
+	$('#showControl').click(function(){
+		if ($('#control').is(":visible")){
+			$('#control').hide(400);
+			$('#showControl').text("Show Admin Controls");
+		} else{
+			$('#control').show(400);
+			$('#showControl').text("Hide Admin Controls");
+		}
+	});
 });
 
 function update(event){
