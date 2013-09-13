351abaf26f0ef7c7c894b2257fe7ffcfc1c9c52a
OpenShift guest <eaglesmaysoarbutweaslesdontgetsuckedintojetengines@hotmail.com>
2013-09-13 06:11:20 -0400

    general update

--- targets3.0/index.php
+++ targets3.0/index.php
@@ -2,6 +2,7 @@
 	<head>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="js/main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style/style.css">
 	</head>
 	<body>
 	
@@ -11,27 +12,26 @@
 			$targets = explode("," , $temp[0]);
 			$remaining = explode("," , $temp[1]);
 		?>
-		<h1>Testing if push to Git works</h1>
 		<div class='peerReview'>
 			<h2>Peer Review</h2>
 			<span id='peerReviewTarget'>Target: <?php echo $targets[0]; ?></span><br/>
 			<span id='peerReviewRemaining'>Remaining: <?php echo $remaining[0]; ?></span><br/>
 			<input type='text' id='peerReviewAmount' placeholder='1'/><br/>
-			<a id='peerReviewButton' href='action.php?task=peerReview&amount=1'>Decrement</a>
+			<a id='peerReviewButton' href='action.php?task=peerReview&amount=1' class='button'>Decrement</a>
 		</div>
 		<div class='editing'>
 			<h2>Editing</h2>
 			<span id='peerReviewTarget'>Target: <?php echo $targets[1]; ?></span><br/>
 			<span id='peerReviewRemaining'>Remaining: <?php echo $remaining[1]; ?></span><br/>
 			<input type='text' id='editingAmount' placeholder='1'/><br/>
-			<a href='action.php?task=editing&amount=1' id='editingButton'>Decrement</a>
+			<a href='action.php?task=editing&amount=1' id='editingButton' class='button'>Decrement</a>
 		</div>
 		<div class='proofing'>
 			<h2>Proofing</h2>
 			<span id='peerReviewTarget'>Target: <?php echo $targets[2]; ?></span><br/>
 			<span id='peerReviewRemaining'>Remaining: <?php echo $remaining[2]; ?></span><br/>
 			<input type='text' id='proofingAmount' placeholder='1'/><br/>
-			<a href='action.php?task=proofing&amount=1' id='proofingButton'>Decrement</a>
+			<a href='action.php?task=proofing&amount=1' id='proofingButton' class='button'>Decrement</a>
 		</div>
 	</body>
 	
