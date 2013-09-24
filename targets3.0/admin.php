a938546ed0fc0426942ac84027d32e8a599ec160
OpenShift guest <eaglesmaysoarbutweaslesdontgetsuckedintojetengines@hotmail.com>
2013-09-24 05:04:54 -0400

    table on admin page

--- targets3.0/admin.php
+++ targets3.0/admin.php
@@ -5,35 +5,33 @@
 	</head>
 	
 	<body>
-	<a href='index.php'>Main page</a>
+	<a class='button' href='index.php'>Main page</a>
 	<?php 
 	    $teams = array("Analytical", "Biological", "General", "Inorganic", "Interfaces","Materials","Organic","Organic Applications", "Physical");
 	    $teamCodes = array("ana", "bio", "gen", "inorg", "int", "mat", "org", "app", "phys");
 	    
+	    echo "<table border='1'>";
+		    echo "<tr>";
+		        echo "<th>Team</th>";
+		        echo "<th>Peer Review</th>";
+		        echo "<th>Editing</th>";
+		        echo "<th>Proofing</th>";
+		    echo "</tr>";
+		    
 	    for($i = 0; $i < count($teams); $i++){
 	        $vals = file_get_contents("files/".$teamCodes[$i] .".php");
 	        $temp = explode (";", $vals);
 		    $targets = explode("," , $temp[0]);
 		    $remaining = explode("," , $temp[1]);
 		    
-		    echo "<div class='team'>".$teams[$i];
-		    echo "<div class='counter'>Peer Review<br/>";
-		    echo $remaining[0]."/".$targets[0]; 
-		    echo "</div>";
-		    echo "<div class='counter'>Editing<br/>";
-		    echo $remaining[1]."/".$targets[1]; 
-		    echo "</div>";
-		    echo "<div class='counter'>Proofing<br/>";
-		    echo $remaining[2]."/".$targets[2]; 
-		    echo "</div>";
-		    echo "</div>";
+		    echo "<tr>";
+		        echo "<td>".$teams[$i]."</td>";
+		        echo "<td>".$remaining[0]."/".$targets[0]."</td>"; 
+		        echo "<td>".$remaining[1]."/".$targets[1]."</td>"; 
+		        echo "<td>".$remaining[2]."/".$targets[2]."</td>"; 
+		    echo "</tr>";
 	    }
-	    
-	    
-		$vals = file_get_contents("files/ana.php");
-		$temp = explode (";", $vals);
-		$targets = explode("," , $temp[0]);
-		$remaining = explode("," , $temp[1]);		
+	    echo "</table>";
 	?>
 	</body>
 </html>
