3dfc72c3e5ecad2df7573f99fed9fdafad2452e3
OpenShift guest <eaglesmaysoarbutweaslesdontgetsuckedintojetengines@hotmail.com>
2013-09-24 05:20:02 -0400

    totals and % on admin page

--- targets3.0/admin.php
+++ targets3.0/admin.php
@@ -10,6 +10,9 @@
 	    $teams = array("Analytical", "Biological", "General", "Inorganic", "Interfaces","Materials","Organic","Organic Applications", "Physical");
 	    $teamCodes = array("ana", "bio", "gen", "inorg", "int", "mat", "org", "app", "phys");
 	    
+	    $totalTarget;
+	    $totalRemaining;
+	    
 	    echo "<table border='1'>";
 		    echo "<tr>";
 		        echo "<th>Team</th>";
@@ -24,6 +27,13 @@
 		    $targets = explode("," , $temp[0]);
 		    $remaining = explode("," , $temp[1]);
 		    
+		    $totalTarget[0] += $targets[0];
+		    $totalTarget[1] += $targets[1];
+		    $totalTarget[2] += $targets[2];
+		    $totalRemaining[0] += $remaining[0];
+		    $totalRemaining[1] += $remaining[1];
+		    $totalRemaining[2] += $remaining[2];
+		    
 		    echo "<tr>";
 		        echo "<td>".$teams[$i]."</td>";
 		        echo "<td>".$remaining[0]."/".$targets[0]."</td>"; 
@@ -31,7 +41,26 @@
 		        echo "<td>".$remaining[2]."/".$targets[2]."</td>"; 
 		    echo "</tr>";
 	    }
+	        echo "<tr>";
+	            echo "<th>Totals</th>";
+	            echo "<td>".$totalRemaining[0]."/".$totalTarget[0]."</td>";
+	            echo "<td>".$totalRemaining[1]."/".$totalTarget[1]."</td>";
+	            echo "<td>".$totalRemaining[2]."/".$totalTarget[2]."</td>";
+	        echo "</tr>";
+	        
+	        $prP = round((1 - $totalRemaining[0]/$totalTarget[0])*100, 2);
+	        $eP = round((1 - $totalRemaining[1]/$totalTarget[1])*100, 2);
+	        $pP = round((1 - $totalRemaining[2]/$totalTarget[2])*100, 2);
+	        
+	        echo "<tr>";
+	            echo "<th>% Complete</th>";
+	            echo "<td>".$prP."</td>";
+	            echo "<td>".$eP."</td>";
+	            echo "<td>".$pP."</td>";
+	        echo "</tr>";
 	    echo "</table>";
+	    
+	    
 	?>
 	</body>
 </html>
